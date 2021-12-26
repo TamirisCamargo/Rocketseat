a58959463c8c7aae1b13fae1f2af62a0c35a617c
EngageTamiris <87433670+TamirisCamargo@users.noreply.github.com>
2021-12-26 17:47:40 -0300

    "JavaScript"

---
+++ PRATICANDO/declaration.js
@@ -0,0 +1,32 @@
+// DECLARATION ASSIGNMRNT VAR
+
+// declaração or declaration 
+var name 
+
+// assigment or atribuição de valores 
+name = "Mike"
+
+//que tipo de dado foi colocado na variável
+//console.log(typeof name)
+
+// AGRUPANDO DECLARAÇÕES
+
+let age, isHuman 
+
+age = 18
+isHuman = true
+
+console.log(name, age, isHuman)
+
+// CONCATENANDO E INTERPOLANDO VARIÁVEIS
+
+// multiplos argumentos na função
+// console.log(name, age, isHuman)
+
+// escrita de textos + variáveis
+
+// concatenando os valores
+// console.log('o ' + name + ' tem' + age + ' anos.')
+
+// interpolando valores com template literals or template strings
+console.log(`o ${name} tem ${age} anos.`)
---
+++ arrays.js
@@ -0,0 +1,13 @@
+// Arrays 
+
+const animals = [
+    'Lion',
+    'Monkey',
+    {
+        name: 'Cat',
+        age: 3
+    }
+]
+
+// acessar valores dentro do Array
+console.log(animals)
