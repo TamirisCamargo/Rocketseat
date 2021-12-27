0d8ddd333ebaee185bea221a5f53dffd710f77e3
EngageTamiris <87433670+TamirisCamargo@users.noreply.github.com>
2021-12-26 23:37:51 -0300

    JavaScript

---
+++ expressoesEOperadores/expressoesEoperadores.js
@@ -0,0 +1,18 @@
+/* Expressões e Operadores 
+
+- Expressions
+- Operadors
+    Binary
+    Unary
+    Ternary
+*/
+
+let number = 1; 
+
+(function() {
+    console.log('alo')
+})()
+
+let number = 1; 
+
+console.log(++number);
